3031e685055194073fbaaae0bbf976b7dd7a4060
rusmyhal <migalruslann@gmail.com>
2020-01-26 16:50:56 +0200

    fetch currencies every second

--- app/src/main/java/com/rusmyhal/rates/feature/currencies/data/CurrenciesRepository.kt
+++ app/src/main/java/com/rusmyhal/rates/feature/currencies/data/CurrenciesRepository.kt
@@ -1,7 +1,25 @@
 package com.rusmyhal.rates.feature.currencies.data
 
+import com.rusmyhal.rates.feature.currencies.data.entity.CurrencyRate
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+
 class CurrenciesRepository(private val currenciesApiService: CurrenciesApiService) {
 
-    suspend fun fetchCurrencies(baseCurrency: String? = null) =
-        currenciesApiService.getCurrencies(baseCurrency)
+    companion object {
+        private const val FETCHING_INTERVAL_IN_MILLIS = 1000L
+    }
+
+    suspend fun fetchCurrenciesRates(baseCurrency: String? = null): Flow<List<CurrencyRate>> =
+        flow {
+            while (true) {
+                emit(currenciesApiService.getCurrencies(baseCurrency).currenciesRates
+                    .map { entry ->
+                        CurrencyRate(entry.key, entry.value)
+                    })
+                delay(FETCHING_INTERVAL_IN_MILLIS)
+            }
+        }
 }
---
+++ app/src/main/java/com/rusmyhal/rates/feature/currencies/data/entity/CurrencyRate.kt
@@ -0,0 +1,3 @@
+package com.rusmyhal.rates.feature.currencies.data.entity
+
+data class CurrencyRate(val currencyCode: String, val rate: Float)
