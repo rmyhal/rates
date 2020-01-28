ef4c5ac46cfa02cbda3e41eab7a6b9aafe6c1661
rusmyhal <migalruslann@gmail.com>
2020-01-28 21:02:04 +0200

    cover currencies viewmodel with unit testing

--- app/src/test/java/com/rusmyhal/rates/TestUtil.kt
+++ app/src/test/java/com/rusmyhal/rates/TestUtil.kt
@@ -1,12 +1,25 @@
 package com.rusmyhal.rates
 
 import com.rusmyhal.rates.feature.currencies.data.entity.CurrenciesResponse
+import com.rusmyhal.rates.feature.currencies.data.entity.Currency
 import com.rusmyhal.rates.feature.currencies.data.entity.CurrencyRate
 
 object TestUtil {
 
     val CURRENCY_RATE_1 = CurrencyRate("USD", 1.1234f)
     val CURRENCY_RATE_2 = CurrencyRate("GBP", 0.7812f)
+
+    val DEFAULT_CURRENCY = Currency("EUR", "1.00", 1)
+    val CURRENCY_1 = Currency(CURRENCY_RATE_1.code, "1.12", 1)
+    val CURRENCY_2 = Currency(CURRENCY_RATE_2.code, "0.78", 1)
+
+    val DEFAULT_CURRENCY_X10 = Currency("EUR", "10.00", 1)
+    val CURRENCY_1_X10 = Currency(CURRENCY_RATE_1.code, "11.23", 1)
+    val CURRENCY_2_X10 = Currency(CURRENCY_RATE_2.code, "7.81", 1)
+
+    val DEFAULT_CURRENCY_X0 = Currency("EUR", "", 1)
+    val CURRENCY_1_X0 = Currency(CURRENCY_RATE_1.code, "", 1)
+    val CURRENCY_2_X0 = Currency(CURRENCY_RATE_2.code, "", 1)
 
     val CURRENCY_RESPONSE_1 =
         CurrenciesResponse("EUR", mapOf(CURRENCY_RATE_1.code to CURRENCY_RATE_1.rate))
--- app/src/test/java/com/rusmyhal/rates/feature/currencies/CurrenciesViewModelTest.kt
+++ app/src/test/java/com/rusmyhal/rates/feature/currencies/CurrenciesViewModelTest.kt
@@ -1,27 +1,39 @@
 package com.rusmyhal.rates.feature.currencies
 
-import com.nhaarman.mockitokotlin2.anyOrNull
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.verify
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.nhaarman.mockitokotlin2.*
+import com.rusmyhal.rates.TestSchedulers
+import com.rusmyhal.rates.TestUtil
 import com.rusmyhal.rates.core.ResourcesManager
 import com.rusmyhal.rates.feature.currencies.data.CurrenciesRepository
+import com.rusmyhal.rates.feature.currencies.data.entity.Currency
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.ObsoleteCoroutinesApi
+import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.mockito.ArgumentMatchers.anyString
 
 @ObsoleteCoroutinesApi
 @ExperimentalCoroutinesApi
 class CurrenciesViewModelTest {
 
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
     private val repository: CurrenciesRepository = mock()
     private val resourcesManager: ResourcesManager = mock()
+    private val currenciesObserver: Observer<List<Currency>> = mock()
+    private val networkErrorObserver: Observer<String?> = mock()
+    private val schedulers = TestSchedulers()
 
     private val mainThreadSurrogate = newSingleThreadContext("UI thread")
 
@@ -30,7 +42,10 @@ class CurrenciesViewModelTest {
     @Before
     fun setup() {
         Dispatchers.setMain(mainThreadSurrogate)
-        viewModel = CurrenciesViewModel(repository, resourcesManager)
+        viewModel = CurrenciesViewModel(repository, resourcesManager, schedulers).apply {
+            currencies.observeForever(currenciesObserver)
+            networkErrorMessage.observeForever(networkErrorObserver)
+        }
     }
 
     @After
@@ -40,8 +55,77 @@ class CurrenciesViewModelTest {
     }
 
     @Test
-    fun testStartStop() = runBlockingTest {
+    fun fetchCurrencies_success() = runBlockingTest {
+        whenever(resourcesManager.getCurrencyFlagResByCode(any())).thenReturn(1)
+        whenever(repository.fetchCurrenciesRates(anyOrNull())).thenReturn(
+            listOf(
+                listOf(
+                    TestUtil.CURRENCY_RATE_1,
+                    TestUtil.CURRENCY_RATE_2
+                )
+            ).asFlow()
+        )
+
         viewModel.startUpdatingCurrencies()
-        verify(repository).fetchCurrenciesRates(anyOrNull())
+        verify(repository).fetchCurrenciesRates(TestUtil.DEFAULT_CURRENCY.code)
+        verify(currenciesObserver).onChanged(
+            listOf(
+                TestUtil.DEFAULT_CURRENCY,
+                TestUtil.CURRENCY_1,
+                TestUtil.CURRENCY_2
+            )
+        )
+        // no errors
+        verifyNoMoreInteractions(networkErrorObserver)
+    }
+
+    @Test
+    fun selectDefaultCurrency_doNothing() = runBlockingTest {
+        viewModel.selectCurrency(TestUtil.DEFAULT_CURRENCY)
+        verifyNoMoreInteractions(repository)
+    }
+
+    @Test
+    fun selectNewCurrency() = runBlockingTest {
+        val currenciesList = listOf(TestUtil.CURRENCY_RATE_1, TestUtil.CURRENCY_RATE_2)
+        whenever(repository.fetchCurrenciesRates(anyOrNull())).thenReturn(listOf(currenciesList).asFlow())
+
+        viewModel.startUpdatingCurrencies()
+        verify(repository).fetchCurrenciesRates(TestUtil.DEFAULT_CURRENCY.code)
+
+        viewModel.selectCurrency(TestUtil.CURRENCY_1)
+        verify(repository).fetchCurrenciesRates(TestUtil.CURRENCY_1.code)
+    }
+
+    @Test
+    fun updateAmount_aboveZero() = runBlockingTest {
+        val currenciesList = listOf(TestUtil.CURRENCY_RATE_1, TestUtil.CURRENCY_RATE_2)
+        whenever(repository.fetchCurrenciesRates(anyOrNull())).thenReturn(listOf(currenciesList).asFlow())
+        whenever(resourcesManager.getCurrencyFlagResByCode(anyString())).thenReturn(1)
+
+        val newAmount = "10.00"
+        viewModel.startUpdatingCurrencies()
+        verify(currenciesObserver).onChanged(any())
+
+        viewModel.onAmountChanged(newAmount)
+
+        val convertedCurrenciesList = listOf(TestUtil.DEFAULT_CURRENCY_X10, TestUtil.CURRENCY_1_X10, TestUtil.CURRENCY_2_X10)
+        verify(currenciesObserver).onChanged(convertedCurrenciesList)
+    }
+
+    @Test
+    fun updateAmount_zero() = runBlockingTest {
+        val currenciesList = listOf(TestUtil.CURRENCY_RATE_1, TestUtil.CURRENCY_RATE_2)
+        whenever(repository.fetchCurrenciesRates(anyOrNull())).thenReturn(listOf(currenciesList).asFlow())
+        whenever(resourcesManager.getCurrencyFlagResByCode(anyString())).thenReturn(1)
+
+        val newAmount = "0"
+        viewModel.startUpdatingCurrencies()
+        verify(currenciesObserver).onChanged(any())
+
+        viewModel.onAmountChanged(newAmount)
+
+        val convertedCurrenciesList = listOf(TestUtil.DEFAULT_CURRENCY_X0, TestUtil.CURRENCY_1_X0, TestUtil.CURRENCY_2_X0)
+        verify(currenciesObserver).onChanged(convertedCurrenciesList)
     }
 }
