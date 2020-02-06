d9db0dff727c623c564403724c140298aefd28cc
rusmyhal <migalruslann@gmail.com>
2020-02-06 20:19:50 +0200

    replace float with bigdecimal for better calculations

--- app/src/main/java/com/rusmyhal/rates/feature/currencies/data/entity/CurrenciesResponse.kt
+++ app/src/main/java/com/rusmyhal/rates/feature/currencies/data/entity/CurrenciesResponse.kt
@@ -1,8 +1,9 @@
 package com.rusmyhal.rates.feature.currencies.data.entity
 
 import com.google.gson.annotations.SerializedName
+import java.math.BigDecimal
 
 data class CurrenciesResponse(
     @SerializedName("base") val baseCurrency: String,
-    @SerializedName("rates") val currenciesRates: Map<String, Float>
+    @SerializedName("rates") val currenciesRates: Map<String, BigDecimal>
 )
